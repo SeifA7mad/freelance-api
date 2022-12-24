b5509d6494a5fb65c019eb7cfe259933512d4c18
SeifA7mad <sauofa_ahmad@yahoo.com>
2022-12-24 14:39:03 +0200

    Update ZodValidationPipe.ts

--- src/pipe/ZodValidationPipe.ts
+++ src/pipe/ZodValidationPipe.ts
@@ -12,16 +12,22 @@ export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
   transform(value: any, metadata: ArgumentMetadata) {
-    const result = this.schema.safeParse(value);
-    if (result.success === false) {
-      const errs = [...result.error.formErrors.formErrors];
-
-      Object.keys(result.error.formErrors.fieldErrors).forEach((key) => {
-        errs.push(`(${key}) ${result.error.formErrors.fieldErrors[key]}`);
-      });
-
-      throw new BadRequestException(errs);
+    try {
+      this.schema.parse(value);
+    } catch (e) {
+      throw new BadRequestException(e.errors);
     }
+
+    // const result = this.schema.safeParse(value);
+    // if (result.success === false) {
+    //   const errs = [...result.error.formErrors.formErrors];
+
+    //   Object.keys(result.error.formErrors.fieldErrors).forEach((key) => {
+    //     errs.push(`(${key}) ${result.error.formErrors.fieldErrors[key]}`);
+    //   });
+
+    //   throw new BadRequestException(errs);
+    // }
     return value;
   }
 }
